33a611cf783adf0c62d8e9ed19c62740c8ee31c0
Rudyyz <mrudimustakim@gmail.com>
2025-10-21 17:58:57 +0700

    push

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Halo, ini proyek pertama saya di Codelab 1!");
+        System.out.println("Nama saya Rudi");
     }
 }

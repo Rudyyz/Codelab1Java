9e59eef2596aeecbb8be40622edaa2f73c1649b1
Rudyyz <mrudimustakim@gmail.com>
2025-10-22 13:58:45 +0700

    push

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,47 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Halo, ini proyek pertama saya di Codelab 1!");
-        System.out.println("Nama saya Rudi"):
+        Scanner input = new Scanner(System.in);
+        double angka1, angka2, hasil;
+        char operator;
+
+        System.out.println("=== Kalkulator Sederhana ===");
+        System.out.print("Masukkan angka pertama: ");
+        angka1 = input.nextDouble();
+
+        System.out.print("Masukkan operator (+, -, *, /): ");
+        operator = input.next().charAt(0);
+
+        System.out.print("Masukkan angka kedua: ");
+        angka2 = input.nextDouble();
+
+        switch (operator) {
+            case '+':
+                hasil = angka1 + angka2;
+                System.out.println("Hasil: " + hasil);
+                break;
+            case '-':
+                hasil = angka1 - angka2;
+                System.out.println("Hasil: " + hasil);
+                break;
+            case '*':
+                hasil = angka1 * angka2;
+                System.out.println("Hasil: " + hasil);
+                break;
+            case '/':
+                if (angka2 != 0) {
+                    hasil = angka1 / angka2;
+                    System.out.println("Hasil: " + hasil);
+                } else {
+                    System.out.println("Error: Pembagian dengan nol tidak diperbolehkan!");
+                }
+                break;
+            default:
+                System.out.println("Operator tidak valid!");
+                break;
+        }
+
+        input.close();
     }
 }
